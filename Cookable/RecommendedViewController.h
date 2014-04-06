1f967f4fa5da85bb244fd37bdfa9ae804a6883f8
Alex Wolff <A.Leon.Wolff@gmail.com>
2014-04-06 17:10:52 -0400

    collections working!

---
+++ Cookable/RecommendedViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecommendedViewController.h
+//  Cookable
+//
+//  Created by Alex Wolff on 4/6/14.
+//  Copyright (c) 2014 Alex Wolff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecommendedViewController : UIViewController
+
+@end
---
+++ Cookable/ResultsCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultsCollectionViewController.h
+//  Cookable
+//
+//  Created by Alex Wolff on 4/6/14.
+//  Copyright (c) 2014 Alex Wolff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsCollectionViewController : UICollectionViewController
+
+@end
