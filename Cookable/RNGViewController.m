efb7a8ec621db17eb4c1dc685300206db33cad63
kwadwo Nyarko <kwadwon@gmail.com>
2014-04-07 17:50:04 -0400

    updated order page view

--- Cookable/RNGViewController.m
+++ Cookable/RNGViewController.m
@@ -16,8 +16,11 @@
 
 
 @implementation RNGViewController
-@synthesize label_quant_1,label_quant_2,label_quant_3,label_quant_4,label_quant_5, label_price_1,label_price_2,
-label_price_3,label_price_4,label_price_5,stepper_1;
+@synthesize label_quant_1,label_quant_2,label_quant_3,label_quant_4,label_quant_5,label_quant_6,
+label_quant_7,label_quant_8,label_quant_9;
+
+@synthesize label_price_1,label_price_2,label_price_3,label_price_4,label_price_5,label_price_6,
+label_price_7,label_price_8,label_price_9,total_price,place_order;
 
 - (void)viewDidLoad
 {
@@ -37,6 +40,7 @@ label_price_3,label_price_4,label_price_5,stepper_1;
 
     label_quant_1.text = [numberFormatter stringFromNumber:[NSNumber numberWithFloat:sender.value]];
     label_price_1.text = [NSString stringWithFormat:@"$ %.02f", sender.value * 5];
+    [self updatePrice];
 }
 
 -(IBAction)update2:(UIStepper*)sender
@@ -45,6 +49,8 @@ label_price_3,label_price_4,label_price_5,stepper_1;
 
     label_quant_2.text = [numberFormatter stringFromNumber:[NSNumber numberWithFloat:sender.value]];
     label_price_2.text = [NSString stringWithFormat:@"$ %.02f", sender.value * 4];
+    [self updatePrice];
+
 }
 
 -(IBAction)update3:(UIStepper*)sender
@@ -53,7 +59,86 @@ label_price_3,label_price_4,label_price_5,stepper_1;
 
     label_quant_3.text = [numberFormatter stringFromNumber:[NSNumber numberWithFloat:sender.value]];
     label_price_3.text = [NSString stringWithFormat:@"$ %.02f", sender.value * 3];
+    [self updatePrice];
+
 }
+
+-(IBAction)update4:(UIStepper*)sender
+{
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    
+    label_quant_4.text = [numberFormatter stringFromNumber:[NSNumber numberWithFloat:sender.value]];
+    label_price_4.text = [NSString stringWithFormat:@"$ %.02f", sender.value * 6];
+    [self updatePrice];
+
+}
+
+-(IBAction)update5:(UIStepper*)sender
+{
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    
+    label_quant_5.text = [numberFormatter stringFromNumber:[NSNumber numberWithFloat:sender.value]];
+    label_price_5.text = [NSString stringWithFormat:@"$ %.02f", sender.value * 5];
+    [self updatePrice];
+
+}
+
+-(IBAction)update6:(UIStepper*)sender
+{
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    
+    label_quant_6.text = [numberFormatter stringFromNumber:[NSNumber numberWithFloat:sender.value]];
+    label_price_6.text = [NSString stringWithFormat:@"$ %.02f", sender.value * 7];
+    [self updatePrice];
+
+}
+
+-(IBAction)update7:(UIStepper*)sender
+{
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    
+    label_quant_7.text = [numberFormatter stringFromNumber:[NSNumber numberWithFloat:sender.value]];
+    label_price_7.text = [NSString stringWithFormat:@"$ %.02f", sender.value * 4];
+    [self updatePrice];
+
+}
+
+-(IBAction)update8:(UIStepper*)sender
+{
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    
+    label_quant_8.text = [numberFormatter stringFromNumber:[NSNumber numberWithFloat:sender.value]];
+    label_price_8.text = [NSString stringWithFormat:@"$ %.02f", sender.value * 5];
+    [self updatePrice];
+
+}
+
+-(IBAction)update9:(UIStepper*)sender
+{
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    
+    label_quant_9.text = [numberFormatter stringFromNumber:[NSNumber numberWithFloat:sender.value]];
+    label_price_9.text = [NSString stringWithFormat:@"$ %.02f", sender.value * 3];
+    [self updatePrice];
+
+}
+
+-(IBAction)updatePrice
+{
+    total_price.text = [NSString stringWithFormat:@"$ %0.2f", [label_quant_9.text floatValue]*3 + [label_quant_8.text floatValue]*5 + [label_quant_7.text floatValue]*4 + [label_quant_6.text floatValue]*7 + [label_quant_5.text floatValue]*5 + [label_quant_4.text floatValue]*6 + [label_quant_3.text floatValue]*3 + [label_quant_2.text floatValue]*4 + [label_quant_1.text floatValue]*5 ];
+
+}
+
+
+-(IBAction)showConfirmation
+{
+    
+}
+
+
+
+
+
 
 
 @end
