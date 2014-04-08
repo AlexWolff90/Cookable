4851836d81e4085e7d9210acf94c0bddac47000a
Kevin Luo <kevkluo@gmail.com>
2014-04-07 21:35:43 -0400

    details pages

---
+++ Cookable/StepViewController.h
@@ -0,0 +1,13 @@
+//
+//  StepViewController.h
+//  Cookable
+//
+//  Created by Kevin Luo on 4/7/14.
+//  Copyright (c) 2014 Alex Wolff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StepViewController : UIViewController
+
+@end
