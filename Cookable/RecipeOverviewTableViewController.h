c8c419847c29a29e3720be225d626e22db7e6919
Alex Wolff <A.Leon.Wolff@gmail.com>
2014-04-08 19:07:53 -0400

    alt ing

--- Cookable/RecipeOverviewTableViewController.h
+++ Cookable/RecipeOverviewTableViewController.h
@@ -9,31 +9,25 @@
 #import <UIKit/UIKit.h>
 
 @interface RecipeOverviewTableViewController : UITableViewController
-    <UIPickerViewDelegate, UIPickerViewDataSource, UIGestureRecognizerDelegate>
-{
-    UIPickerView       *onionPicker;
-    UIPickerView       *olivePicker;
-    UIPickerView       *lemonPicker;
-    NSArray            *onionAlts;
-    NSArray            *oliveAlts;
-    NSArray            *lemonAlts;
-    UILabel            *onionLabel;
-    UILabel            *oliveLabel;
-    UILabel            *lemonLabel;
-    UITableView        *ingredients;
-}
-@property (nonatomic, retain) IBOutlet UIPickerView *onionPicker;
-@property (nonatomic, retain) IBOutlet UIPickerView *olivePicker;
-@property (nonatomic, retain) IBOutlet UIPickerView *lemonPicker;
-@property (nonatomic, retain) IBOutlet UILabel *onionLabel;
-@property (nonatomic, retain) IBOutlet UILabel *oliveLabel;
-@property (nonatomic, retain) IBOutlet UILabel *lemonLabel;
-@property (nonatomic, retain) NSArray *onionAlts;
-@property (nonatomic, retain) NSArray *oliveAlts;
-@property (nonatomic, retain) NSArray *lemonAlts;
-@property (strong, nonatomic) IBOutlet UITableView *ingredients;
-- (IBAction)onionLeft:(UISwipeGestureRecognizer *)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *onionLabel;
 - (IBAction)onionRight:(UISwipeGestureRecognizer *)sender;
+- (IBAction)onionLeft:(UISwipeGestureRecognizer *)sender;
+- (IBAction)onionRightB:(UIButton *)sender;
+- (IBAction)onionLeftB:(UIButton *)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *oliveLabel;
+- (IBAction)oliveRight:(UISwipeGestureRecognizer *)sender;
+- (IBAction)oliveLeft:(UISwipeGestureRecognizer *)sender;
+- (IBAction)oliveRightB:(UIButton *)sender;
+- (IBAction)oliveLeftB:(UIButton *)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *lemonLabel;
+- (IBAction)lemonRight:(UISwipeGestureRecognizer *)sender;
+- (IBAction)lemonLeft:(UISwipeGestureRecognizer *)sender;
+- (IBAction)lemonLeftB:(UIButton *)sender;
+- (IBAction)lemonRightB:(UIButton *)sender;
+
 
 
 
--- Cookable/RecipeOverviewTableViewController.m
+++ Cookable/RecipeOverviewTableViewController.m
@@ -15,9 +15,9 @@
 
 
 @implementation RecipeOverviewTableViewController
-@synthesize onionPicker, olivePicker, lemonPicker, onionAlts, oliveAlts, lemonAlts;
-@synthesize onionLabel, oliveLabel, lemonLabel;
-@synthesize ingredients;
+@synthesize onionLabel;
+@synthesize oliveLabel;
+@synthesize lemonLabel;
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -41,13 +41,7 @@
     // Initialize recipe image array
     recipeImages = [NSArray arrayWithObjects:@"Greek-Salad.jpg", @"greek-salad-su-1173749-l.jpg",
                     @"greek-salad-520-a.jpg", @"Greek-Salad-2.jpg", nil];
-   
-    self.onionAlts = [[NSArray alloc] initWithObjects:
-                         @"1/2 Small Red Onion", @"1/2 Small Green Onion", nil];
-    self.oliveAlts = [[NSArray alloc] initWithObjects:
-                      @"1 Talbespoon Olive Oil", @"1 Tablespoon Grape Seed Oil", nil];
-    self.onionAlts = [[NSArray alloc] initWithObjects:
-                      @"1 Teaspoon Lemon Juice", @"1 Teaspoon Lime Juice", nil];
+  
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
@@ -92,56 +86,6 @@
     } else {
         return 6;
     }
-}
-
-#pragma mark -
-#pragma mark PickerView DataSource
-
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
-{
-    return 1;
-}
-
-- (NSInteger)pickerView:(UIPickerView *)pickerView
-numberOfRowsInComponent:(NSInteger)component
-{
-    return 2;
-}
-
-- (NSString *)pickerView:(UIPickerView *)pickerView
-             titleForRow:(NSInteger)row
-            forComponent:(NSInteger)component
-{
-    if([pickerView isEqual: onionPicker]){
-        return [onionAlts objectAtIndex:row];
-    }else if([pickerView isEqual: olivePicker]){
-        return [oliveAlts objectAtIndex:row];
-    }else return [lemonAlts objectAtIndex:row];
-}
-
-#pragma mark -
-#pragma mark PickerView Delegate
--(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row
-      inComponent:(NSInteger)component
-{
-    if([pickerView isEqual: onionPicker]){
-        onionLabel.text = [onionAlts objectAtIndex:row];
-    }else if([pickerView isEqual: olivePicker]){
-        oliveLabel.text = [oliveAlts objectAtIndex:row];
-    }else lemonLabel.text = [lemonAlts objectAtIndex:row];
-}
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    if (indexPath.row == 3) {
-        if (onionPicker) onionPicker.hidden = !onionPicker.hidden;
-    } else if (indexPath.row == 6){
-        if (olivePicker) olivePicker.hidden = !olivePicker.hidden;
-    } else if (indexPath.row == 7){
-        if (lemonPicker) lemonPicker.hidden = !lemonPicker.hidden;
-    }
-    
 }
 
 /*
@@ -205,8 +149,48 @@ numberOfRowsInComponent:(NSInteger)component
 */
 
 - (IBAction)onionLeft:(UISwipeGestureRecognizer *)sender {
+    onionLabel.text = @"1/2 Small Green Onion";
+}
+
+- (IBAction)onionRightB:(UIButton *)sender {
+    onionLabel.text = @"1/2 Small Red Onion";
+}
+
+- (IBAction)onionLeftB:(UIButton *)sender {
+    onionLabel.text = @"1/2 Small Green Onion";
 }
 
 - (IBAction)onionRight:(UISwipeGestureRecognizer *)sender {
+    onionLabel.text = @"1/2 Small Red Onion";
+}
+- (IBAction)oliveRight:(UISwipeGestureRecognizer *)sender {
+    oliveLabel.text = @"1 Tablespoon Olive Oil";
+}
+
+- (IBAction)oliveLeft:(UISwipeGestureRecognizer *)sender {
+    oliveLabel.text = @"1 Tablespoon Grapeseed Oil";
+}
+
+- (IBAction)oliveRightB:(UIButton *)sender {
+    oliveLabel.text = @"1 Tablespoon Olive Oil";
+}
+
+- (IBAction)oliveLeftB:(UIButton *)sender {
+    oliveLabel.text = @"1 Tablespoon Grapeseed Oil";
+}
+- (IBAction)lemonRight:(UISwipeGestureRecognizer *)sender {
+    lemonLabel.text = @"1 Teaspoon Lemon Juice";
+}
+
+- (IBAction)lemonLeft:(UISwipeGestureRecognizer *)sender {
+    lemonLabel.text = @"1 Teaspoon Lime Juice";
+}
+
+- (IBAction)lemonLeftB:(UIButton *)sender {
+    lemonLabel.text = @"1 Teaspoon Lime Juice";
+}
+
+- (IBAction)lemonRightB:(UIButton *)sender {
+    lemonLabel.text = @"1 Teaspoon Lemon Juice";
 }
 @end
