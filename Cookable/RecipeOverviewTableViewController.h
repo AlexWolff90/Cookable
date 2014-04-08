e7135ca922e6435acc319964c2510224dd643ea3
Alex Wolff <A.Leon.Wolff@gmail.com>
2014-04-07 21:34:56 -0400

    meh

--- Cookable/RecipeOverviewTableViewController.h
+++ Cookable/RecipeOverviewTableViewController.h
@@ -9,6 +9,32 @@
 #import <UIKit/UIKit.h>
 
 @interface RecipeOverviewTableViewController : UITableViewController
-        
+    <UIPickerViewDelegate, UIPickerViewDataSource, UIGestureRecognizerDelegate>
+{
+    UIPickerView       *onionPicker;
+    UIPickerView       *olivePicker;
+    UIPickerView       *lemonPicker;
+    NSArray            *onionAlts;
+    NSArray            *oliveAlts;
+    NSArray            *lemonAlts;
+    UILabel            *onionLabel;
+    UILabel            *oliveLabel;
+    UILabel            *lemonLabel;
+    UITableView        *ingredients;
+}
+@property (nonatomic, retain) IBOutlet UIPickerView *onionPicker;
+@property (nonatomic, retain) IBOutlet UIPickerView *olivePicker;
+@property (nonatomic, retain) IBOutlet UIPickerView *lemonPicker;
+@property (nonatomic, retain) IBOutlet UILabel *onionLabel;
+@property (nonatomic, retain) IBOutlet UILabel *oliveLabel;
+@property (nonatomic, retain) IBOutlet UILabel *lemonLabel;
+@property (nonatomic, retain) NSArray *onionAlts;
+@property (nonatomic, retain) NSArray *oliveAlts;
+@property (nonatomic, retain) NSArray *lemonAlts;
+@property (strong, nonatomic) IBOutlet UITableView *ingredients;
+- (IBAction)onionLeft:(UISwipeGestureRecognizer *)sender;
+- (IBAction)onionRight:(UISwipeGestureRecognizer *)sender;
+
+
 
 @end
